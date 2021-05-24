f4194b700e08890a3eeb562ee4e2eba17540476c
Azu Barraza <azucena.barraza@wizeline.com>
2021-05-24 15:19:59 -0500

    chore: learn about arrays and tuples

--- notes/1-basics.ts
+++ notes/1-basics.ts
@@ -51,27 +51,27 @@ zz = 41;
 /**
  * (8) simple array types can be expressed using []
  */
-// let aa: number[] = [];
-// aa.push(33);
+let aa: number[] = [];
+aa.push(33);
 // aa.push("abc"); // 🚨 ERROR: Argument of type '"abc"' is not assignable to parameter of type 'number'.
 
 /**
  * (9) we can even define a tuple, which has a fixed length
  */
-// let bb: [number, string, string, number] = [
-//   123,
-//   "Fake Street",
-//   "Nowhere, USA",
-//   10110
-// ];
+let bb: [number, string, string, number] = [
+  123,
+  "Fake Street",
+  "Nowhere, USA",
+  10110
+];
 
 // bb = [1, 2, 3]; // 🚨 ERROR: Type 'number' is not assignable to type 'string'.
 
 /**
  * (10) Tuple values often require type annotations (  : [number, number] )
  */
-// const xx = [32, 31]; // number[];
-// const yy: [number, number] = [32, 31];
+const xx = [32, 31]; // number[];
+const yy: [number, number] = [32, 31];
 
 //== OBJECTS ==//
 
