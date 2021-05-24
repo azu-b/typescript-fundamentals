9823c90371f607890217c9d1fd6b8d35f31dd311
Azu Barraza <azucena.barraza@wizeline.com>
2021-05-24 15:40:56 -0500

    chore: learn about object typing and interfaces intro

--- notes/1-basics.ts
+++ notes/1-basics.ts
@@ -78,11 +78,11 @@ const yy: [number, number] = [32, 31];
 /**
  * (11) object types can be expressed using {} and property names
  */
-// let cc: { houseNumber: number; streetName: string };
-// cc = {
-//   streetName: "Fake Street",
-//   houseNumber: 123
-// };
+let cc: { houseNumber: number; streetName: string };
+cc = {
+  streetName: "Fake Street",
+  houseNumber: 123
+};
 
 // cc = {
 //   houseNumber: 33
@@ -97,18 +97,18 @@ const yy: [number, number] = [32, 31];
  * (12) You can use the optional operator (?) to
  * indicate that something may or may not be there
  */
-// let dd: { houseNumber: number; streetName?: string };
-// dd = {
-//   houseNumber: 33
-// };
+let dd: { houseNumber: number; streetName?: string };
+dd = {
+  houseNumber: 33
+};
 
 // (13) if we want to re-use this type, we can create an interface
-// interface Address {
-//   houseNumber: number;
-//   streetName?: string;
-// }
-// // and refer to it by name
-// let ee: Address = { houseNumber: 33 };
+interface Address {
+  houseNumber: number;
+  streetName?: string;
+}
+// and refer to it by name
+let ee: Address = { houseNumber: 33 };
 
 //== UNION & INTERSECTION ==//
 
